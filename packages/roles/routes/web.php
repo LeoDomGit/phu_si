4074ccd0c26b3fd76bb44ff9098c534a94104579
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-17 10:28:07 +0700

    Commit

--- packages/roles/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use Leo\Roles\Controllers\RolesController;
-use App\Http\Middleware\CheckLogin;
-
-Route::middleware(['web', CheckLogin::class])->group(function () {
-Route::resource('roles', RolesController::class);
-});
